d5c50b4fb3d392d7e48511fe6c069df1ce801832
Markus Broecker <broecker@wisc.edu>
2016-07-19 14:35:52 -0500

    Added mission metadata example file and renderer

---
+++ htdocs/scripts/spatial-metadata.js
@@ -0,0 +1,100 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2016 Markus Broecker <broecker@wisc.edu>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+var metadata = metadata || {};
+
+metadata.load = function(jsonUrl) {
+
+	metadata.items = [];
+
+	// parse json file here
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function () {
+		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+
+			// remove all newline
+			var items = xmlhttp.response.replace(/(\r\n|\n|\r)/gm,"");
+
+			metadata.items = JSON.parse(items);
+
+
+			// parse the data
+			for (var i in metadata.items) {
+
+				// create bounding boxes
+				metadata.items[i].bbox = aabb.create(metadata.items[i].bbox_min, metadata.items[i].bbox_max);
+				delete metadata.items[i].bbox_min; 
+				delete metadata.items[i].bbox_max;
+					
+
+
+
+			}
+	
+
+			console.log("Loaded " + metadata.items.length + " metadata entries.");
+			console.log(metadata.items);
+		}
+	}
+
+	xmlhttp.open("GET", jsonUrl, true)
+	xmlhttp.send();
+
+
+}
+
+
+metadata.draw = function(shader) {
+	if (!(shader === null)) {
+
+		gl.lineWidth(3);
+
+		gl.useProgram(shader);
+		gl.enableVertexAttribArray(shader.vertexPositionAttribute);
+		gl.uniform3f(shader.colorUniform, 1, 1, 0);
+
+
+		for (var e in metadata.items) { 
+			
+			var item = metadata.items[e];
+			
+			if (item.bbox) {
+				aabb.drawAABB(item.bbox, shader);
+
+				//console.log(item.bbox);
+
+			}
+		}
+
+		// draw text ehere
+
+
+
+		gl.lineWidth(1.0);
+
+
+	}
+
+
+}
